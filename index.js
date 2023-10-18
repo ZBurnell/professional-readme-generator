a9d236a52b4d8fa041ddf51ab11ab2cf60277cde
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-10-17 22:16:11 -0400

    Added prompts for each section

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ Title , Description , Installation , Usage , Credits , License , Features , Contribute];
+
+const prompt = inquirer.createPromptModule();
+inquirer.prompt([
+{
+   type: "input",
+   message: "What do you want to call your app",
+   name: "Title",
+},
+{
+  type: "input",
+  message: "give a brief description of your app",
+  name: "Description",
+},
+{
+  type: "input",
+  message: "How does a user access your app(URL or Link to download)?",
+  name: "Installation",
+},
+{
+  type: "input",
+  message: "Did you get code or help from anyone you need to give credit to?",
+  name: "Credits",
+},
+{
+  type: "list",
+  message: "Choose a license for your Repo",
+  name: "License",
+  choices: ["MIT", "Apache", "GPL"]
+},
+{
+  type: "input",
+  message: "Did you add anything cool the world should know about??",
+  name: "Features",
+},
+{
+    type: "list",
+    message: "Do you want people to be able to contribute to your app?",
+    name: "License",
+    choices: ["OH YEAH", "NAH"]
+  },
+])
+.then((answers) => {
+const README = generatedREADME(questions)
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(InputsREADME, data) {}
-
+fs.writeFile('README.md', generatedREADME, (err) => {
+    if (err) {
+      console.error('Error creating your file:', err);
+    } else {
+      console.log('All data added to file successfully.');
+    }
+  })
+});
 // TODO: Create a function to initialize app
 function init() {}
 
